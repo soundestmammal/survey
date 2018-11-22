c4e50550a300e78ea2695bc304fa3ac385381f38
Robert Checco <robert.checco@gmail.com>
2018-11-21 19:59:27 -0500

    Initial Commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '657229618839-8jr4r0kt8b6nqu11a4vcpfm1503mt28q.apps.googleusercontent.com',
+    googleClientSecret: 'tlNTw5nGCk8tvaZBFdCGvFy-',
+    mongoURI: 'mongodb://mongo:mongoose1@ds211694.mlab.com:11694/emaily-dev',
+    cookieKey: 'fjdsalhfiasjfiojasdfhkwh'
+}
