43d6efdcf1ea360e90c861a87bc99bd14b33b6df
Robert Checco <robert.checco@gmail.com>
2019-02-23 20:21:51 -0500

    React-Router set up with initial routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+
+const Dashboard = () => <div> This is the dashboard</div>
+
+
+const SurveyNew = () => <div>this is the survey new</div> 
+
+
+const Landing = () => <div> this is the landing</div>
+
 
 const App = () => {
-    return <div>This is the App.js Component</div>
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing}/>
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
